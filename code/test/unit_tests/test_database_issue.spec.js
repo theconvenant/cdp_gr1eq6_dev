6e17549d1734f2caa8c2c1f350077e9d4cd2bc42
Florian Lajoinie <flo.laj@sfr.fr>
2019-11-26 17:30:09 +0100

    [task 302 - US12-13-14] correction des tests d'insertions pour les issues

--- code/test/unit_tests/test_database_issue.spec.js
+++ code/test/unit_tests/test_database_issue.spec.js
@@ -1,4 +1,6 @@
-const database = require('../../db_controller/issue_db')
+const dbIssue = require('../../db_controller/issue_db')
+const dbUser = require('../../db_controller/user_db')
+const dbProject = require('../../db_controller/project_db')
 
 var assert = require('assert')
 const description = 'Description'
@@ -6,32 +8,37 @@ const difficulty = 1
 const priority = 'Priority'
 const usNum = 'UsNum'
 const state = 'State'
-const projectId = -1
 
 describe('Test insertIssue', function () {
-    database.insertIssue(description, difficulty, priority, usNum, state, projectId).then(function () {
-        database.findListIssuesByProjectID(projectId).then(function (issue) {
-            it('should return the issue\'s description', function () {
-                assert.equal(description, issue.description)
-            })
-            it('should return the issue\'s difficulty', function () {
-                assert.equal(difficulty, issue.difficulty)
-            })
-            it('should return the issue\'s priority', function () {
-                assert.equal(priority, issue.priority)
-            })
-            it('should return the issue\'s priority', function () {
-                assert.equal(usNum, issue.us_num)
-            })
-            it('should return the issue\'s priority', function () {
-                assert.equal(state, issue.test_state)
-            })
-            it('should return the issue\'s priority', function () {
-                assert.equal(projectId, issue._project_id)
-            })
-            database.deleteIssue(projectId, issue._issue_id).then(function () {
-                database.findListIssuesByProjectID(projectId).then(function (deletedIssue) {
-                    assert.equal(deletedIssue, undefined)
+    const userName = 'userName'
+    const email = 'email@test.com'
+    const password = 'password'
+    const projectName = 'projectName'
+    let projectId = -1
+    it('test_create_issue', function () {
+        dbUser.insertUser(userName, email, password).then(function () {
+            dbProject.insertProject(projectName, userName, description).then(function () {
+                dbProject.findListProjectsByOwnerName(userName).then(function (project) {
+                    projectId = project[0]._project_id
+                    dbIssue.insertIssue(description, difficulty, priority, usNum, state, projectId).then(function () {
+                        dbIssue.findListIssuesByProjectID(projectId).then(function (issue) {
+                            const issueID = issue[0]._issue_id
+                            assert.equal(description, issue[0].description)
+                            assert.equal(difficulty, issue[0].difficulty)
+                            assert.equal(priority, issue[0].priority)
+                            assert.equal(usNum, issue[0].us_num)
+                            assert.equal(state, issue[0].test_state)
+                            assert.equal(projectId, issue[0]._project_id)
+                            assert.notEqual(-1, issue[0]._project_id)
+                            dbIssue.deleteIssue(projectId, issueID).then(function () {
+                                dbIssue.findListIssuesByProjectID(projectId).then(function (deletedIssue) {
+                                    assert.equal(deletedIssue[0], undefined)
+                                    dbUser.deleteAccount(userName)
+                                    // dbProject.deleteProject(projectName, userName)
+                                })
+                            })
+                        })
+                    })
                 })
             })
         })
