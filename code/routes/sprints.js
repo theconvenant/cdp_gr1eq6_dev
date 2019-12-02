68a10651c7d0a88162fa47aa0e653a787dcb0ad5
Antoine Gourgue <antoine33124@hotmail.fr>
2019-12-02 17:51:14 +0100

    Task 315 - Ajout du bouton addSprint et affichage de la liste des sprints

--- code/routes/sprints.js
+++ code/routes/sprints.js
@@ -1,5 +1,8 @@
+const sprintDb = require('../db_controller/sprint_db')
+
 var projectId
 var projectName
+const moment = require('moment');
 
 module.exports = function (app) {
     this.setProjectId = function (id, name) {
@@ -9,6 +12,17 @@ module.exports = function (app) {
     app.get('/sprints',
         require('connect-ensure-login').ensureLoggedIn(),
         function (req, res) {
-            res.render('sprints', { idProject: projectId, projectName: projectName })
+            sprintDb.findSprintsByProjectId(projectId).then (sprintList => {
+                res.render('sprints', {moment: moment, idProject: projectId, projectName: projectName, sprintList: sprintList})
+            })
+        })
+        app.post('/insertSprint', 
+        require('connect-ensure-login').ensureLoggedIn(),
+        function (req, res) {
+            sprintDb.insertSprint(req.body.sprintName, req.body.startDate, req.body.endDate, projectId, req.body.description).then( 
+                ()=>{
+                    res.redirect('/sprints')
+                }
+            )
         })
 }
