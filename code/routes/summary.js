e48f9dbf9102f34710c34038c22f64b8e3ca6117
Antoine Gourgue <antoine33124@hotmail.fr>
2019-12-10 15:06:49 +0100

    Task 318 et 332 - modification du sprint et ajout des progress bars dans summary

--- code/routes/summary.js
+++ code/routes/summary.js
@@ -1,3 +1,6 @@
+const sprintDb = require('../db_controller/sprint_db')
+const taskDb = require('../db_controller/task_db')
+
 var projectId
 var projectName
 
@@ -9,6 +12,43 @@ module.exports = function (app) {
     app.get('/summary',
         require('connect-ensure-login').ensureLoggedIn(),
         function (req, res) {
-            res.render('summary', { idProject: projectId, projectName: projectName })
+            sprintDb.findSprintsByProjectId(projectId).then( sprintsList => {
+                const sprintPercentageList = getListSprintIssues(projectId, sprintsList)
+                res.render('summary', { idProject: projectId, projectName: projectName, 
+                    sprintsList:sprintsList, sprintPercentageList: sprintPercentageList })
+            })
         })
+        function getListSprintIssues (projectId, sprintsList) {
+            if(!sprintsList){
+                return null
+            }
+            var sprintPercentageList = new Map()
+            sprintsList.forEach(
+                sprint => {
+                    var nbTasks = 0
+                    var nbDoneTasks = 0
+                    sprintDb.findIssueListOfSprint(sprint._id, projectId).then(
+                        issues => {
+                            issues.forEach( 
+                                issue => {
+                                    taskDb.findTaskListOfIssue(issue._issue_id, projectId).then(
+                                        tasksList => {
+                                            tasksList.forEach(
+                                                task => {
+                                                    ++nbTasks
+                                                    if(task.state == 'DONE')
+                                                    ++nbDoneTasks
+                                                }
+                                            )
+                                        }
+                                    )
+                                }
+                            )
+                        }
+                    )
+                sprintPercentageList.set(sprint._id, (nbDoneTasks/nbTasks*100))
+                }
+            )
+            return sprintPercentageList
+        }
 }
